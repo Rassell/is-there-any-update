e13fce5e470bee76147f38773840829c21e31531
Gonzalo Soriano <sordesos@gmail.com>
2022-02-06 19:59:58 +0100

    title for window

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ function createWindow() {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        title: 'Is there any update?',
         webPreferences: {
             nodeIntegration: false,
             devTools: isDev,
